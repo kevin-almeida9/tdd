74a8f1425673cb4af9f32d7235a026f1ffb687e8
Kevin Almeida <kevin.almeida@agxsoftware.com>
2023-09-27 17:58:53 -0300

    lesson 17

--- src/components/__tests__/WeatherCurrent.test.tsx
+++ src/components/__tests__/WeatherCurrent.test.tsx
@@ -7,4 +7,8 @@ describe('WeatherCurrent', () => {
     const wrapper = render(<WeatherCurrent />);
     wrapper.getByTestId('weather-current');
   });
+
+  test('Should navigate to weather screen', () => {
+    throw new Error('Test no implamented');
+  });
 });
